4ba2c3341fda518fca5af40f8ac8186a8ce5ca2d
gilles.dehaudt <gilles.dehaudt@3bf79e3c-a79d-667f-db61-7bf86d50bc88>
2012-03-09 09:02:57 +0000

    improve mod security gestion

--- trunk/bin/update-rules.sh
+++ trunk/bin/update-rules.sh
@@ -35,60 +35,6 @@ cp -rf optional_rules  $CONFDIR/security-rules/
 cp -rf slr_rules $CONFDIR/security-rules/
 cp -rf experimental_rules $CONFDIR/security-rules/
 
-echo "Dumping rules into database... "
-rm -f /tmp/.vulture-rules
-#WE ARE IGNORING inbound and outbound blocking rules 'cause they are managed trough Vulture
-for i in `find $CONFDIR/security-rules/base_rules/ -name *.conf |grep -v bound_blocking`; do
-	echo "Include $i" >> /tmp/.vulture-rules
-done
-data=`cat /tmp/.vulture-rules`
-BASE_RULE=`sqlite3 $BASEDIR/admin/db "SELECT count(*) from modsecurity where name='RULES: OWASP ModSecurity Core - BASE'"`
-if [ $BASE_RULE = 0 ]; then
-	CMD="INSERT INTO modsecurity (name, rules) VALUES ('RULES: OWASP ModSecurity Core - BASE','$data')";
-else
-	CMD="UPDATE modsecurity SET rules='$data' WHERE name='RULES: OWASP ModSecurity Core - BASE'";
-fi
-sqlite3 $BASEDIR/admin/db "$CMD" || exit
-
-rm -f /tmp/.vulture-rules
-for i in `find $CONFDIR/security-rules/optional_rules/ -name *.conf`; do
-	echo "Include $i" >> /tmp/.vulture-rules
-done
-data=`cat /tmp/.vulture-rules`
-OPT_RULE=`sqlite3 $BASEDIR/admin/db "SELECT count(*) from modsecurity where name='RULES: OWASP ModSecurity Core - OPTIONAL'"`
-if [ $BASE_RULE = 0 ]; then
-	CMD="INSERT INTO modsecurity (name, rules) VALUES ('RULES: OWASP ModSecurity Core - OPTIONAL','$data')";
-else
-	CMD="UPDATE modsecurity SET rules='$data' WHERE name='RULES: OWASP ModSecurity Core - OPTIONAL'";
-fi
-sqlite3 $BASEDIR/admin/db "$CMD" || exit
-
-rm -f /tmp/.vulture-rules
-for i in `find $CONFDIR/security-rules/slr_rules/ -name *.conf`; do
-	echo "Include $i" >> /tmp/.vulture-rules
-done
-data=`cat /tmp/.vulture-rules`
-SLR_RULE=`sqlite3 $BASEDIR/admin/db "SELECT count(*) from modsecurity where name='RULES: OWASP ModSecurity Core - SpiderLabs Research'"`
-if [ $SLR_RULE = 0 ]; then
-	CMD="INSERT INTO modsecurity (name, rules) VALUES ('RULES: OWASP ModSecurity Core - SpiderLabs Research','$data')";
-else
-	CMD="UPDATE modsecurity SET rules='$data' WHERE name='RULES: OWASP ModSecurity Core - SpiderLabs Research'";
-fi
-sqlite3 $BASEDIR/admin/db "$CMD" || exit
-
-rm -f /tmp/.vulture-rules
-for i in `find $CONFDIR/security-rules/experimental_rules/ -name *.conf`; do
-	echo "Include $i" >> /tmp/.vulture-rules
-done
-data=`cat /tmp/.vulture-rules`
-EXP_RULE=`sqlite3 $BASEDIR/admin/db "SELECT count(*) from modsecurity where name='RULES: OWASP ModSecurity Core - EXPERIMENTAL'"`
-if [ $EXP_RULE = 0 ]; then
-	CMD="INSERT INTO modsecurity (name, rules) VALUES ('RULES: OWASP ModSecurity Core - EXPERIMENTAL','$data')";
-else
-	CMD="UPDATE modsecurity SET rules='$data' WHERE name='RULES: OWASP ModSecurity Core - EXPERIMENTAL'";
-fi
-sqlite3 $BASEDIR/admin/db "$CMD" || exit
-
 rm -rf /tmp/vulture$$
 rm -f /tmp/.vulture-rules
 
