9b69536bb460d5dcd9095c206874f827b00dd660
etiennehelluy@gmail.com <etiennehelluy@gmail.com@3bf79e3c-a79d-667f-db61-7bf86d50bc88>
2013-01-04 13:11:15 +0000

    balancer is now ok

--- trunk/admin/vulture/templatetags/split.py
+++ trunk/admin/vulture/templatetags/split.py
@@ -4,9 +4,11 @@ from django.template.defaultfilters import stringfilter
 register = template.Library()
 
 @register.filter
-def mysplit(value, sep = "."):
-    if sep not in value:
-        return (value,)
+def mysplit(value, sep):
     parts = value.split(sep)
     return (parts[0], sep.join(parts[1:]))
 
+@register.filter
+def mysplitbalancer(value, sep):
+    parts = [x.strip() for x in value.split(sep)]
+    return parts
