84292adb6bb009bc8a3e67b845263208a15aff7a
etiennehelluy <etiennehelluy@gmail.com>
2013-01-04 10:44:49 +0000

    fix a bug in balancer config generation

--- admin/vulture/templatetags/split.py
+++ admin/vulture/templatetags/split.py
@@ -5,6 +5,8 @@ register = template.Library()
 
 @register.filter
 def mysplit(value, sep = "."):
+    if sep not in value:
+        return (value,)
     parts = value.split(sep)
     return (parts[0], sep.join(parts[1:]))
 
