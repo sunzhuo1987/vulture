32fbbb1b603816f4ae05cb564d38879a9c0c593a
gilles.dehaudt <gilles.dehaudt@gmail.com>
2013-04-19 10:00:21 +0000

    Redirect no log works as wanted

--- lib/Vulture/Plugin/Plugin_REDIRECT_NO_LOG.pm
+++ lib/Vulture/Plugin/Plugin_REDIRECT_NO_LOG.pm
@@ -19,34 +19,37 @@ sub plugin {
     my ( $package_name, $r, $log, $dbh, $intf, $app, $options ) = @_;
     $log->debug("########## Plugin_REDIRECT_NO_LOG ##########");
     my $mc_conf = $r->pnotes('mc_conf');
+    my $cookie_SSO_name = $r->dir_config('VultureProxyCookieName');
     my $cookie_app_name = $r->dir_config('VultureAppCookieName');
     #Getting SSO session if exists.
     my $SSO_cookie_name =
       Core::VultureUtils::get_cookie( $r->headers_in->{Cookie},
-         $cookie_app_name . '=([^;]*)' );
+         $cookie_SSO_name . '=([^;]*)' );
     my (%session_SSO);
 
     Core::VultureUtils::session( \%session_SSO, $intf->{sso_timeout},
         $SSO_cookie_name, $log, $mc_conf, $intf->{sso_update_access_time} );
-    unless ( $session_SSO{'is_auth'} and $r->hostname =~ $intf->{'sso_portal'} )
+$log->debug("session is auth is ".$session_SSO{'is_auth'}." cookie is $SSO_cookie_name and host is ".$r->hostname.$r->unparsed_uri);
+    unless ( $session_SSO{'is_auth'} )
     {
         $log->debug("Redirect");
         if ( $r->unparsed_uri =~ /$cookie_app_name=([^;]*)/ ) {
             my $app_cookie_name = $1;
             my (%session_app);
-
+		$log->debug('toto');
             #Get app
             Core::VultureUtils::session( \%session_app, $app->{timeout},
                 $app_cookie_name, $log, $mc_conf, $app->{update_access_time} );
             my $app = Core::VultureUtils::get_app( $log, $dbh, $mc_conf,
                     $intf->{id}, $session_app{app_name});
-            $log->debug( "app is " . $session_app{app_name} );
+            $log->debug( "app is " . $session_app{app_name} . "id is  $app_cookie_name");
 
             #my @list_options = $options;
             my %hash_options = split /,|=>/, $options;
             my ( $k, $v );
             while ( ( $k, $v ) = each(%hash_options) ) {
 
+			$log->debug("$k,$v");
                 #Redirect
                 if ( trim( $session_app{app_name} ) eq trim($k) ) {
                     $r->pnotes( 'response_content' =>
@@ -54,7 +57,6 @@ sub plugin {
                           . $v
                           . "'\"/></head></html>");
                     $r->pnotes( 'response_content_type' => 'text/html' );
-
                     $r->set_handlers( PerlAuthenHandler => undef );
                     $r->set_handlers( PerlAuthzHandler  => undef );
                     $r->set_handlers( PerlFixupHandler  => undef );
