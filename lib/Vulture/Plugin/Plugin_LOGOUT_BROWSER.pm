4e80b4aa50e962014b21e6bc3046ee600d0d6e70
etiennehelluy <etiennehelluy@3bf79e3c-a79d-667f-db61-7bf86d50bc88>
2013-06-11 15:00:19 +0000

    use custom headers when forwarding logout

--- lib/Vulture/Plugin/Plugin_LOGOUT_BROWSER.pm
+++ lib/Vulture/Plugin/Plugin_LOGOUT_BROWSER.pm
@@ -19,11 +19,32 @@ use Apache2::Reload;
 use Core::VultureUtils
   qw(&get_cookie &session &get_memcached &set_memcached &notify &get_translations &get_style);
 use Apache2::Const -compile => qw(OK FORBIDDEN REDIRECT);
+use Apache::SSLLookup;
 use LWP::UserAgent;
 
 sub plugin {
     my ( $package_name, $r, $log, $dbh, $intf, $app, $options ) = @_;
     $r = Apache::SSLLookup->new($r);
+    my %headers_vars = (
+        2  => 'SSL_CLIENT_I_DN',
+        3  => 'SSL_CLIENT_M_SERIAL',
+        4  => 'SSL_CLIENT_S_DN',
+        5  => 'SSL_CLIENT_V_START',
+        6  => 'SSL_CLIENT_V_END',
+        7  => 'SSL_CLIENT_S_DN_C',
+        8  => 'SSL_CLIENT_S_DN_ST',
+        9  => 'SSL_CLIENT_S_DN_Email',
+        10 => 'SSL_CLIENT_S_DN_L',
+        11 => 'SSL_CLIENT_S_DN_O',
+        12 => 'SSL_CLIENT_S_DN_OU',
+        13 => 'SSL_CLIENT_S_DN_CN',
+        14 => 'SSL_CLIENT_S_DN_T',
+        15 => 'SSL_CLIENT_S_DN_I',
+        16 => 'SSL_CLIENT_S_DN_G',
+        17 => 'SSL_CLIENT_S_DN_S',
+        18 => 'SSL_CLIENT_S_DN_D',
+        19 => 'SSL_CLIENT_S_DN_UID',
+    );
     my $mc_conf = $r->pnotes('mc_conf');
 
     $log->debug("########## Plugin_LOGOUT_BROWSER ".$app->{name}."##########");
@@ -45,10 +66,39 @@ sub plugin {
         }
         #Create logout request
         my $request = HTTP::Request->new( 'GET', $url . $logout_url );
-        $request->push_header('User-Agent' => $r->headers_in->{'User-Agent'} );
-        $request->push_header('Cookie' => $cookies );
-#        $request->push_header('Host' => $host );
 
+        #Setting headers
+        $request->remove_header('User-Agent');
+        $request->push_header('User-Agent' => $r->headers_in->{'User-Agent'});
+        #Pushing cookies
+        $request->remove_header( 'Cookie');
+        $request->push_header( 'Cookie' => $r->headers_in->{'Cookie'});
+
+        my $sth =
+          $dbh->prepare("SELECT name, type, value FROM header WHERE app_id= ?");
+        $sth->execute( $app->{id} );
+
+        #Push specific headers to get the right form
+        while ( my ( $h_name, $h_type, $h_value ) = $sth->fetchrow ) {
+            if ( $h_type eq "REMOTE_ADDR" ) {
+                $h_value = $r->connection->remote_ip;
+                #Nothing to do
+            }
+            elsif ( $h_type eq "CUSTOM" ) {
+                #Types related to SSL
+            }
+            else {
+                $h_value = $r->ssl_lookup( $headers_vars{$h_type} )
+                  if ( exists $headers_vars{$h_type} );
+            }
+            #Try to push custom headers
+            eval {
+                $log->debug("Pushing custom header $h_name => $h_value");
+                $request->remove_header($h_name);
+                $request->push_header($h_name => $h_value);
+            };
+        }
+        $sth->finish();
         #Send logout request
         $ua->request($request);
     }
