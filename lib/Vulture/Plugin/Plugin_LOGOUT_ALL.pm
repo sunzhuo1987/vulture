e9953a520e819658aace88d5242acf756c95cf75
etiennehelluy <etiennehelluy@gmail.com>
2013-08-21 09:46:41 +0000

    add wrappers to create LWP::UserAgent and HTTP::Request for a specific application

--- lib/Vulture/Plugin/Plugin_LOGOUT_ALL.pm
+++ lib/Vulture/Plugin/Plugin_LOGOUT_ALL.pm
@@ -17,40 +17,15 @@ BEGIN {
 use Apache2::Log;
 use Apache2::Reload;
 use Core::VultureUtils
-  qw(&get_cookie &session &get_memcached &set_memcached &notify &get_translations &get_style &parse_set_cookie);
+  qw(&get_cookie &session &get_memcached &set_memcached &notify &get_translations &get_style &parse_set_cookie &get_ua_object &get_http_request);
 use Apache2::Const -compile => qw(OK FORBIDDEN REDIRECT);
-use Apache::SSLLookup;
-use LWP::UserAgent;
 
 sub plugin {
     my ( $package_name, $r, $log, $dbh, $intf, $app, $options ) = @_;
-    $r = Apache::SSLLookup->new($r);
     my $mc_conf = $r->pnotes('mc_conf');
-    my %headers_vars = (
-        2  => 'SSL_CLIENT_I_DN',
-        3  => 'SSL_CLIENT_M_SERIAL',
-        4  => 'SSL_CLIENT_S_DN',
-        5  => 'SSL_CLIENT_V_START',
-        6  => 'SSL_CLIENT_V_END',
-        7  => 'SSL_CLIENT_S_DN_C',
-        8  => 'SSL_CLIENT_S_DN_ST',
-        9  => 'SSL_CLIENT_S_DN_Email',
-        10 => 'SSL_CLIENT_S_DN_L',
-        11 => 'SSL_CLIENT_S_DN_O',
-        12 => 'SSL_CLIENT_S_DN_OU',
-        13 => 'SSL_CLIENT_S_DN_CN',
-        14 => 'SSL_CLIENT_S_DN_T',
-        15 => 'SSL_CLIENT_S_DN_I',
-        16 => 'SSL_CLIENT_S_DN_G',
-        17 => 'SSL_CLIENT_S_DN_S',
-        18 => 'SSL_CLIENT_S_DN_D',
-        19 => 'SSL_CLIENT_S_DN_UID',
-    );
-
     $log->debug("########## Plugin_LOGOUT_ALL ##########");
 
     #Taking user identity
-
     my ($id_app,$id_sso);
     my (%session_app,%session_SSO);
     
@@ -80,7 +55,6 @@ sub plugin {
 
     #Foreach app where user is currently logged in
     foreach my $key ( keys %session_SSO ) {
-
         #Reject bad app key
         my @wrong_keys =
           qw/is_auth username url_to_redirect password SSO last_access_time _session_id random_token/;
@@ -108,47 +82,10 @@ sub plugin {
 
             if ( $url and $logout_url ) {
                 #Setting fake user agent
-                my ( $ua, $response, $request );
-                $ua = LWP::UserAgent->new;
-                #Setting proxy if needed
-                if ( $remote_proxy ne '' ) {
-                    $ua->proxy( [ 'http', 'https' ], $remote_proxy );
-                }
-                #Setting request
-                $request = HTTP::Request->new( 'GET', $url . $logout_url );
-                #Setting headers
-                $request->remove_header('User-Agent');
-                $request->push_header(
-                    'User-Agent' => $r->headers_in->{'User-Agent'} );
-                #Pushing cookies
-                $request->remove_header( 'Cookie');
-                $request->push_header('Cookie' => $current_app{cookie}) if ($current_app{cookie});
-                my $sth =
-                  $dbh->prepare("SELECT name, type, value FROM header WHERE app_id= ?");
-                $sth->execute( $app_id );
-                #Push specific headers to get the right form
-                while ( my ( $h_name, $h_type, $h_value ) = $sth->fetchrow ) {
-                    if ( $h_type eq "REMOTE_ADDR" ) {
-                        $h_value = $r->connection->remote_ip;
-                        #Nothing to do
-                    }
-                    elsif ( $h_type eq "CUSTOM" ) {
-                        #Types related to SSL
-                    }
-                    else {
-                        $h_value = $r->ssl_lookup( $headers_vars{$h_type} )
-                          if ( exists $headers_vars{$h_type} );
-                    }
-                    #Try to push custom headers
-                    eval {
-                        $log->debug("Pushing custom header $h_name => $h_value");
-                        $request->remove_header($h_name);
-                        $request->push_header($h_name => $h_value);
-                    };
-                }
-                $sth->finish();
+                my $ua = Core::VultureUtils::get_ua_object($r, $remote_proxy);
+                my $request = Core::VultureUtils::get_http_request($r, $dbh, $app_id, 'GET', $url . $logout_url);
                 #Getting response
-                $response = $ua->request($request);
+                my $response = $ua->request($request);
                 #Render cookie
                 my %cookies_app;
                 if ( $response->headers->header('Set-Cookie') ) {
@@ -180,22 +117,18 @@ sub plugin {
                         'Location' => $response->headers->header('Location') );
                 }
             }
-            #End query
-            $sth->finish();
             #Delete current session
             tied(%current_app)->delete();
         }
     }
     #Logout from SSO
     tied(%session_SSO)->delete();
-    #Debug for eval
-    $log->debug($@) if $@;
     #Destroy useless handlers
     $r->set_handlers( PerlAccessHandler => undef );
     $r->set_handlers( PerlAuthenHandler => undef );
     $r->set_handlers( PerlAuthzHandler  => undef );
     $r->set_handlers( PerlFixupHandler  => undef );
-    my $translations = get_translations( $r, $log, $dbh, "DISCONNECTED" );
+#    my $translations = get_translations( $r, $log, $dbh, "DISCONNECTED" );
 #If no html, send form
 #    my $html = get_style($r, $log, $dbh, $app, 'LOGOUT', 'Logout from Vulture', {FORM => ''}, $translations);
     $options ||= "/";
@@ -206,5 +139,4 @@ sub plugin {
     $r->pnotes( 'response_content_type' => 'text/html' );
     return Apache2::Const::OK;
 }
-
 1;
--- lib/Vulture/Plugin/Plugin_LOGOUT_BROWSER.pm
+++ lib/Vulture/Plugin/Plugin_LOGOUT_BROWSER.pm
@@ -17,34 +17,11 @@ BEGIN {
 use Apache2::Log;
 use Apache2::Reload;
 use Core::VultureUtils
-  qw(&get_cookie &session &get_memcached &set_memcached &notify &get_translations &get_style &parse_cookies);
+  qw(&get_cookie &session &notify &parse_cookies &get_ua_object &get_http_request);
 use Apache2::Const -compile => qw(OK FORBIDDEN REDIRECT);
-use Apache::SSLLookup;
-use LWP::UserAgent;
 
 sub plugin {
     my ( $package_name, $r, $log, $dbh, $intf, $app, $options ) = @_;
-    $r = Apache::SSLLookup->new($r);
-    my %headers_vars = (
-        2  => 'SSL_CLIENT_I_DN',
-        3  => 'SSL_CLIENT_M_SERIAL',
-        4  => 'SSL_CLIENT_S_DN',
-        5  => 'SSL_CLIENT_V_START',
-        6  => 'SSL_CLIENT_V_END',
-        7  => 'SSL_CLIENT_S_DN_C',
-        8  => 'SSL_CLIENT_S_DN_ST',
-        9  => 'SSL_CLIENT_S_DN_Email',
-        10 => 'SSL_CLIENT_S_DN_L',
-        11 => 'SSL_CLIENT_S_DN_O',
-        12 => 'SSL_CLIENT_S_DN_OU',
-        13 => 'SSL_CLIENT_S_DN_CN',
-        14 => 'SSL_CLIENT_S_DN_T',
-        15 => 'SSL_CLIENT_S_DN_I',
-        16 => 'SSL_CLIENT_S_DN_G',
-        17 => 'SSL_CLIENT_S_DN_S',
-        18 => 'SSL_CLIENT_S_DN_D',
-        19 => 'SSL_CLIENT_S_DN_UID',
-    );
     my $mc_conf = $r->pnotes('mc_conf');
 
     $log->debug("########## Plugin_LOGOUT_BROWSER ".$app->{name}."##########");
@@ -52,53 +29,16 @@ sub plugin {
     my $cookies = $r->headers_in->{Cookie};
 
     # Getting server side logout informations
-    my $query = ( "SELECT name, url, remote_proxy, logout_url FROM app WHERE app.name = ?");
+    my $query = ( "SELECT url, remote_proxy, logout_url FROM app WHERE app.name = ?");
     my $sth = $dbh->prepare($query);
     $sth->execute( $app->{'name'} );
-    my ( $host, $url, $remote_proxy, $logout_url ) = $sth->fetchrow_array;
-    $sth->finish();
+    my ( $url, $remote_proxy, $logout_url ) = $sth->fetchrow_array;
 
     # Server side logout
     if ( $url and $logout_url ) {
-        my $ua = LWP::UserAgent->new;
-        if ( $remote_proxy ne '' ) {
-            $ua->proxy( [ 'http', 'https' ], $remote_proxy );
-        }
         #Create logout request
-        my $request = HTTP::Request->new( 'GET', $url . $logout_url );
-
-        #Setting headers
-        $request->remove_header('User-Agent');
-        $request->push_header('User-Agent' => $r->headers_in->{'User-Agent'});
-        #Pushing cookies
-        $request->remove_header( 'Cookie');
-        $request->push_header( 'Cookie' => $r->headers_in->{'Cookie'});
-
-        my $sth =
-          $dbh->prepare("SELECT name, type, value FROM header WHERE app_id= ?");
-        $sth->execute( $app->{id} );
-
-        #Push specific headers to get the right form
-        while ( my ( $h_name, $h_type, $h_value ) = $sth->fetchrow ) {
-            if ( $h_type eq "REMOTE_ADDR" ) {
-                $h_value = $r->connection->remote_ip;
-                #Nothing to do
-            }
-            elsif ( $h_type eq "CUSTOM" ) {
-                #Types related to SSL
-            }
-            else {
-                $h_value = $r->ssl_lookup( $headers_vars{$h_type} )
-                  if ( exists $headers_vars{$h_type} );
-            }
-            #Try to push custom headers
-            eval {
-                $log->debug("Pushing custom header $h_name => $h_value");
-                $request->remove_header($h_name);
-                $request->push_header($h_name => $h_value);
-            };
-        }
-        $sth->finish();
+        my $ua = Core::VultureUtils::get_ua_object($r, $remote_proxy);
+        my $request = Core::VultureUtils::get_http_request($r, $dbh, $app->{id}, 'GET', $url . $logout_url);
         #Send logout request
         $ua->request($request);
     }
