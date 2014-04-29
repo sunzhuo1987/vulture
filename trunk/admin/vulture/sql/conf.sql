e0a03d7dea892d0d299a38325cc9e9b91185a37e
jeremie.jourdin@gmail.com <jeremie.jourdin@gmail.com@3bf79e3c-a79d-667f-db61-7bf86d50bc88>
2014-04-29 08:01:46 +0000

    Cleanup

--- trunk/admin/vulture/sql/conf.sql
+++ trunk/admin/vulture/sql/conf.sql
@@ -1,4 +1,4 @@
-INSERT INTO "conf" VALUES(1,'version','2.0.7');
+INSERT INTO "conf" VALUES(1,'version','2.0.8');
 INSERT INTO "conf" VALUES(2,'name','VULTURE1');
 INSERT INTO "conf" VALUES(3,'version_conf','0');
 INSERT INTO "conf" VALUES(4,'auto_restart','0');
