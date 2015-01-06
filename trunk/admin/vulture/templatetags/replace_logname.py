6ee7147999ef68d5a2371501fa95793e67bf3f7e
jeremie.jourdin@gmail.com <jeremie.jourdin@gmail.com@3bf79e3c-a79d-667f-db61-7bf86d50bc88>
2015-01-06 08:44:03 +0000

    Fix carriage return in tempmlates

--- trunk/admin/vulture/templatetags/replace_logname.py
+++ trunk/admin/vulture/templatetags/replace_logname.py
@@ -1,10 +1,21 @@
 from django import template
 from django.template.defaultfilters import stringfilter
+from django.utils.text import normalize_newlines
+from django.utils.safestring import mark_safe
 
 register = template.Library()
-
 @register.filter
 @stringfilter
 def replace_logname(value, arg):
     """Replace / by _ in logs"""
     return value.replace(arg, '_')
+
+@register.filter
+@stringfilter
+def normal_newlines(text):
+    """
+    Removes all newline characters from a block of text.
+    """
+    normalized_text = normalize_newlines(text)
+    return mark_safe(normalized_text)
+
